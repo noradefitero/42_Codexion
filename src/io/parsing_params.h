969d42bf1b7c98ca59a141024aeb5d5f60266b7d
noradefitero <nora@defitero.com>
2026-08-20 07:43:28 +0200

    includes for functionality of parsing params

--- src/io/parsing_params.h
+++ src/io/parsing_params.h
@@ -6,7 +6,7 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 01:23:48 by dde-fite          #+#    #+#             */
-/*   Updated: 2026/08/17 01:33:05 by dde-fite         ###   ########.fr       */
+/*   Updated: 2026/08/17 23:04:39 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 # define PARSING_PARAMS_H
 
 # include "codexion.h"
+# include "print.h"
+# include "../utils/ft/ft.h"
 
 # include <stdlib.h>
 # include <stdbool.h>
+# include <string.h>
 
 t_scheduler	str_to_scheduler(char const *s);
 int			parse_arguments(t_config *config, int argc, char *argv[]);
