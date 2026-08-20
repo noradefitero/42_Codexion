563741007524380abe4886c169b49b6205ec3bbc
noradefitero <nora@defitero.com>
2026-08-20 07:45:28 +0200

    minor changes in coder getters

--- src/models/coder_setters.c
+++ src/models/coder_setters.c
@@ -6,23 +6,23 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/13 07:52:15 by dde-fite          #+#    #+#             */
-/*   Updated: 2026/08/16 21:46:19 by dde-fite         ###   ########.fr       */
+/*   Updated: 2026/08/18 01:53:06 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "coder.h"
 
-void	coder__set_left_usb(t_coder *self, t_usb *usb)
+void	coder__set_left_usb(t_coder *self, const t_usb *usb)
 {
-	self->__left_usb = usb;
+	self->__left_usb = (t_usb *)usb;
 }
 
-void	coder__set_right_usb(t_coder *self, t_usb *usb)
+void	coder__set_right_usb(t_coder *self, const t_usb *usb)
 {
-	self->__right_usb = usb;
+	self->__right_usb = (t_usb *)usb;
 }
 
-void	coder__set_exit_thread(t_coder *NONNULL self, bool exit_thread)
+void	coder__set_exit_thread(t_coder *NONNULL self, const bool exit_thread)
 {
-	self->__exit_thread = exit_thread;
+	self->__exit_thread = (bool)exit_thread;
 }
