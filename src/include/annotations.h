f6f84bd3b3e36c3443ef9d8f11cd696d2e0730cc
noradefitero <nora@defitero.com>
2026-09-17 04:20:30 +0200

    fix(annotations): changed defines for nullability for better compatibility

--- src/include/annotations.h
+++ src/include/annotations.h
@@ -6,28 +6,20 @@
 /*   By: dde-fite <dde-fite@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 07:35:41 by dde-fite          #+#    #+#             */
-/*   Updated: 2026/08/16 07:39:08 by dde-fite         ###   ########.fr       */
+/*   Updated: 2026/09/17 03:33:37 by dde-fite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ANNOTATIONS_H
 # define ANNOTATIONS_H
 
-# ifdef __nullable
+# if defined(__clang__)
 #  define NULLABLE __nullable
-# else
-#  define NULLABLE
-# endif
-
-# ifdef __nonnull
 #  define NONNULL __nonnull
-# else
-#  define NONNULL
-# endif
-
-# ifdef __null_unspecified
 #  define NULL_UNSPECIFIED __null_unspecified
 # else
+#  define NULLABLE
+#  define NONNULL
 #  define NULL_UNSPECIFIED
 # endif
 
